5c0efe3845c8edae2716bf5d55181a56c8a66f6f
Sergey <Hellboysis666@gmail.com>
2015-11-07 16:34:02 +0300

    - iterface features
    //TODO:
    - think about interface

--- app/src/main/java/com/example/hellb/agni/adapters/ScheduleAdapter.java
+++ app/src/main/java/com/example/hellb/agni/adapters/ScheduleAdapter.java
@@ -2,6 +2,7 @@ package com.example.hellb.agni.adapters;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.hellb.agni.R;
@@ -84,15 +86,13 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.Schedu
             implements View.OnClickListener{
         public TextView textViewTime, textViewLessonName, textViewAuditoryName, textViewLessonType,
                 textViewHalfGroup, textViewTeachName;
+        private CardView clickableCard;
         private int originalHeight = 0;
         private boolean isViewExpanded = false;
-        private TextView yourCustomView;
+        private LinearLayout yourCustomView;
 
         public ScheduleViewHolder(View itemView) {
             super(itemView);
-
-            itemView.setClickable(true);
-            itemView.setOnClickListener(this);
 
             textViewTime = (TextView) itemView.findViewById(R.id.tvTime);
             textViewLessonName = (TextView) itemView.findViewById(R.id.tvLessonName);
@@ -100,7 +100,11 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.Schedu
             textViewLessonType = (TextView) itemView.findViewById(R.id.tvLessonType);
             textViewHalfGroup = (TextView) itemView.findViewById(R.id.tvHalfGroup);
             textViewTeachName = (TextView) itemView.findViewById(R.id.tvTeachName);
-            yourCustomView = (TextView) itemView.findViewById(R.id.tvExample);
+            clickableCard = (CardView) itemView.findViewById(R.id.cardView);
+            yourCustomView = (LinearLayout) itemView.findViewById(R.id.hiddenLayout);
+
+            clickableCard.setClickable(true);
+            clickableCard.setOnClickListener(this);
 
             if (isViewExpanded == false) {
                 // Set Views to View.GONE and .setEnabled(false)
@@ -121,10 +125,10 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.Schedu
                 yourCustomView.setVisibility(View.VISIBLE);
                 yourCustomView.setEnabled(true);
                 isViewExpanded = true;
-                valueAnimator = ValueAnimator.ofInt(originalHeight, originalHeight + (int) (originalHeight * 2.0)); // These values in this method can be changed to expand however much you like
+                valueAnimator = ValueAnimator.ofInt(originalHeight, originalHeight + (int) (originalHeight * 0.95)); // These values in this method can be changed to expand however much you like
             } else {
                 isViewExpanded = false;
-                valueAnimator = ValueAnimator.ofInt(originalHeight + (int) (originalHeight * 2.0), originalHeight);
+                valueAnimator = ValueAnimator.ofInt(originalHeight + (int) (originalHeight * 0.95), originalHeight);
 
                 Animation a = new AlphaAnimation(1.00f, 0.00f); // Fade out
 
